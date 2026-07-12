f6a3d6831cd00ec848a38610f2db1904931b60e9
venkey27 <venkey271200@gmail.com>
2026-07-12 19:52:59 -0400

    terraform

--- locals.tf
+++ locals.tf
@@ -5,7 +5,7 @@ locals {
     private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]  #we just prefer us-east-1  # split create List(String)
     backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
     frontend_alb_listener_arn = data.aws_ssm_parameter.frontend_alb_listener_arn.value
-    alb_listener_arn = var.component == frontend ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
+    alb_listener_arn = var.component == "frontend" ? local.frontend_alb_listener_arn : local.backend_alb_listener_arn
     host_header = var.component == "frontend" ? "${var.project}-${var.environment}.${var.domain_name}" : "${var.component}.backend-alb-${var.environment}.${var.domain_name}"
     common_name = "${var.project}-${var.environment}-${var.component}"
     common_tags = {
