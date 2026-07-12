7677ffaec8ee7f679da442e4170eb27a747539f7
venkey27 <venkey271200@gmail.com>
2026-07-12 16:39:45 -0400

    terraform

---
+++ locals.tf
@@ -0,0 +1,23 @@
+locals {
+    ami_id = data.aws_ami.joindevops.id
+    
+    sg_id = data.aws_ssm_parameter.sg_id.value
+    private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]  #we just prefer us-east-1  # split create List(String)
+    common_name = "${var.project}-${var.environment}-${var.component}"
+    common_tags = {
+        Project = "${var.project}"
+        Environment = "${var.environment}"
+        Terraform = "true"
+    }
+}
+
+# this is how the split function works here private_subnet_ids
+#    [
+#   "subnet-01234567",
+#   "subnet-89abcdef",
+#   "subnet-xyz12345"
+# ]
+
+# vpc_id = data.aws_ssm_parameter.vpc_id.value
+    # catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
+    # backend_alb_listener_arn = data.aws_ssm_parameter.backend_alb_listener_arn.value
---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "component" {
+    type = string
+}
+  
+variable "app_version" {
+    default = "v3"
+}
+
+# variable "zone_id" {
+#     default = "Z07587789JER0QOC5489"
+# }
+
+variable "domain_name" {
+    default = "exptrack.shop"
+}
+
+
